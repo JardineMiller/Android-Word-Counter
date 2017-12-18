d36d3d3d0615f4984cf5da07d6dcc204b7deb1ba
Jardine <jardine.miller@gmail.com>
2017-12-18 14:55:27 +0000

    Initial commit

---
+++ app/src/main/java/com/example/jardine/wordcounter/Word.java
@@ -0,0 +1,17 @@
+package com.example.jardine.wordcounter;
+
+/**
+ * Created by jardine on 18/12/2017.
+ */
+
+public class Word {
+    private String phrase;
+
+    public Word(String phrase) {
+        this.phrase = phrase;
+    }
+
+    public String getPhrase() {
+        return phrase;
+    }
+}
